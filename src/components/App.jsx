5c967041b0f7c9388d8c155b7521f318b4f194c7
zoria93 <artemzora4@gmail.com>
2023-03-13 23:04:54 +0200

    add contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,111 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.addContact();
+    this.reset();
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  addContact = () => {
+    const newContact = {
+      name: this.state.name,
+      id: nanoid(),
+      number: this.state.number,
+    };
+    this.setState(prevState => {
+      return {
+        contacts: [newContact, ...prevState.contacts],
+      };
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form
+          onSubmit={this.handleSubmit}
+          style={{
+            display: 'inline-flex',
+            flexDirection: 'column',
+            border: '1px solid',
+          }}
+        >
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Numbers
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+
+        <label>
+          Find contacts by name
+          <input
+            type="text"
+            name="filter"
+            value={this.state.filter}
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <ul>
+          {this.state.contacts.map(({ name, id, number }) => {
+            return (
+              <li key={id}>
+                {name}: {number}
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
